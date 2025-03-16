8bada412e2f7bd182a27269a82f8280013a5ee0e
[Mateus] <143464157+Mateus-lang@users.noreply.github.com>
2025-03-16 06:55:37 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return ( 
-    <div className="flex items-center justify-center h-screen bg-gray-100">
+    <div className="flex items-center justify-center h-screen">
       <h1 className="p-5 text-red-500">Hello World</h1>
     </div>
    );
