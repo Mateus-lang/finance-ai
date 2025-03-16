5539e6d258336035a487abb8544c71937c356282
[Mateus] <143464157+Mateus-lang@users.noreply.github.com>
2025-03-16 05:52:33 -0300

    chore: add prisma and database setup

---
+++ app/subscription/page.tsx
@@ -0,0 +1,10 @@
+const Subscription = () => {
+    return ( 
+        <div>
+            <h1>Subscription</h1>
+            <p>Subscription page content goes here.</p>
+        </div>
+     );
+}
+ 
+export default Subscription;
---
+++ app/transactions/page.tsx
@@ -0,0 +1,10 @@
+const Transactions = () => {
+    return ( 
+        <div className="flex flex-col gap-4">
+            <h1 className="text-2xl font-bold">Transactions</h1>
+            <p className="text-gray-500">List of transactions</p>
+        </div>
+     );
+}
+ 
+export default Transactions;
